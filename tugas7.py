99fe320741dbb481430128ca68f0ce3431807a8a
IhjaNurhidayad <faujanhidayat72@gmail.com>
2024-11-19 13:04:52 +0700

    Create tugas7.py

---
+++ tugas7.py
@@ -0,0 +1,9 @@
+#Menghitung segitga
+#nama : ihja nurhidayad
+
+print("menghitung segitiga")
+alas = int(input("masukkan alas : "))
+tinggi = int(input("masukkan tinggi :"))
+
+hasil = 0.5 * alas * tinggi
+print(f"hasil dari102 x {alas}x{tinggi} adalah : {hasil}")
